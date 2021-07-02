029accf8fc3ed1b4a04193b680b45fbe12d518ac
akumar65063 <akumar65063@gmail.com>
2021-07-02 10:18:37 +0100

    Added Test Project with test method for the three test sceanrios

---
+++ PromotionEngine/PromotionEngineTest/PromotionEngineTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PromotionEngine.Promotion;
+namespace PromotionEngineTest
+{
+    [TestClass]
+    public class PromotionEngineTest
+    {
+        private PromotionEngine.Promotion.PromotionEngine promotionEngine = new PromotionEngine.Promotion.PromotionEngine();
+
+        private double amountAfterPromotions;
+
+        [TestMethod]
+        public void Test_GetTotalAmountAfterPromotions_With_Scenario_A()
+        {
+            amountAfterPromotions = promotionEngine.GetTotalAmountAfterPromotions();
+
+            Assert.AreEqual(amountAfterPromotions, 100);
+        }
+
+        [TestMethod]
+        public void Test_GetTotalAmountAfterPromotions_With_Scenario_B()
+        {
+            amountAfterPromotions = promotionEngine.GetTotalAmountAfterPromotions();
+
+            Assert.AreEqual(amountAfterPromotions, 370);
+        }
+
+        [TestMethod]
+        public void Test_GetTotalAmountAfterPromotions_With_Scenario_C()
+        {
+
+            amountAfterPromotions = promotionEngine.GetTotalAmountAfterPromotions();
+
+            Assert.AreEqual(amountAfterPromotions, 280);
+            
+        }
+
+    }
+}
