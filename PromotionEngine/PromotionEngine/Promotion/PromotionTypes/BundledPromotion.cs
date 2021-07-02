d9dda422999a2aeeae96ebb725e2fac58c78005b
akumar65063 <akumar65063@gmail.com>
2021-07-02 10:35:54 +0100

    Added class BundledPromotion. This class will be responsible for applying bundled promotions

---
+++ PromotionEngine/PromotionEngine/Promotion/PromotionTypes/BundledPromotion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace PromotionEngine.Promotion.PromotionTypes
+{
+    public class BundledPromotion
+    {
+        private char _itemSKU;
+        private int _itemCountInOneBundle;
+        private double _priceForBundle;
+
+
+        public BundledPromotion(char sku, int itemCount, double priceForBundle)
+        {
+            _itemSKU = sku;
+            _itemCountInOneBundle = itemCount;
+            _priceForBundle = priceForBundle;
+        }
+
+        public double GetPromotionDiscount()
+        {
+            return 0;
+
+        }
+    }
+}
