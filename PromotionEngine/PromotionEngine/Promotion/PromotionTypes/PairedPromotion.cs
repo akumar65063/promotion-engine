5fe7b41c054820cc0e0a66fcb016788c18e23384
akumar65063 <akumar65063@gmail.com>
2021-07-02 11:53:07 +0100

    Added the logic for calculating dicount amount for paired promotion

--- PromotionEngine/PromotionEngine/Promotion/PromotionTypes/PairedPromotion.cs
+++ PromotionEngine/PromotionEngine/Promotion/PromotionTypes/PairedPromotion.cs
@@ -17,9 +17,33 @@ namespace PromotionEngine.Promotion.PromotionTypes
 
 
         //Returns the amount to be discounted after applying
-        //this promotion
+        //paired promotion
         public double GetPromotionDiscount(Order order)
         {
+            double priceOfItem1, priceOfItem2, amountWithoutDiscount, amountWithDiscount;
+            int countOfItem1, countOfItem2, numberOfPairs;
+
+            if (order.Items.FindAll(x => x.SKU == _firstItemSku).Count > 0 && order.Items.FindAll(x => x.SKU == _secondItemSku).Count > 0)
+            {
+
+                priceOfItem1 = order.Items.Find(x => x.SKU == _firstItemSku).Price;
+
+                priceOfItem2 = order.Items.Find(x => x.SKU == _secondItemSku).Price;
+
+                countOfItem1 = order.Items.FindAll(x => x.SKU == _firstItemSku).Count;
+
+                countOfItem2 = order.Items.FindAll(x => x.SKU == _secondItemSku).Count;
+
+                numberOfPairs = Math.Min(countOfItem1, countOfItem2);
+
+                amountWithoutDiscount = (priceOfItem1 + priceOfItem2) * numberOfPairs;
+
+                amountWithDiscount = _priceForPair * numberOfPairs;
+
+                return amountWithoutDiscount - amountWithDiscount;
+
+            }
+
             return 0;
         }
     }
