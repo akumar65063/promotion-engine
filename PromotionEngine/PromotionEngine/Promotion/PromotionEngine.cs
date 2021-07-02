56133f4300c4e09359c774cc927a771e4f2a0a05
akumar65063 <akumar65063@gmail.com>
2021-07-02 01:45:19 +0100

    Added GetTotalAmountAfterPromotions in the promotionengine class to get the amount after applying promotions

---
+++ PromotionEngine/PromotionEngine/Promotion/PromotionEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PromotionEngine.Promotion
+{
+    public class PromotionEngine
+    {
+        public PromotionEngine()
+        {
+        }
+
+        public double GetTotalAmountAfterPromotions()
+        {
+
+            return 0;
+
+        }
+    }
+}
