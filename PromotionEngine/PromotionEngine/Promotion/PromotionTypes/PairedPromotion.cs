1062e457e3217dd44ed4ccebc846870d017ec674
akumar65063 <akumar65063@gmail.com>
2021-07-02 10:46:00 +0100

    Added class PairedPromotion for processing the paired promotions

---
+++ PromotionEngine/PromotionEngine/Promotion/PromotionTypes/PairedPromotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PromotionEngine.Promotion.PromotionTypes
+{
+    public class PairedPromotion
+    {
+        private char _firstItemSku, _secondItemSku;
+        private double _priceForPair;
+
+        public PairedPromotion(char firstItemSku, char secondItemSku, double priceforPair)
+        {
+            _firstItemSku = firstItemSku;
+            _secondItemSku = secondItemSku;
+            _priceForPair = priceforPair;
+        }
+
+        public double GetPromotionDiscount()
+        {
+            return 0;
+        }
+    }
+}
