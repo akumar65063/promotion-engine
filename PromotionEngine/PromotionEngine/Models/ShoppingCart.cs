1926b8ab2334c8b61af32ab36f965d554786c479
akumar65063 <akumar65063@gmail.com>
2021-07-02 01:04:34 +0100

    Added method to fetch the items in the cart

--- PromotionEngine/PromotionEngine/Models/ShoppingCart.cs
+++ PromotionEngine/PromotionEngine/Models/ShoppingCart.cs
@@ -10,5 +10,27 @@ namespace PromotionEngine.Models
         public ShoppingCart()
         {
         }
-    }
+
+        //Fetch the items from the cart
+        public List<Item> GetCartItems()
+        {
+            return new List<Item>() { new Item { SKU='A'},
+                                      //new Item { SKU='A'},
+                                      //new Item { SKU='A'},
+                                      //new Item { SKU='A'},                                   
+                                      //new Item { SKU='A'},
+                                      //new Item { SKU='A'},
+                                      //new Item { SKU='A'},
+
+                                      new Item { SKU='B'},
+                                      //new Item { SKU='B'},
+                                      //new Item { SKU='B'},
+                                      //new Item { SKU='B'},
+                                      //new Item { SKU='B'},
+
+                                      new Item { SKU='C'},
+                                      //new Item { SKU='C'},
+                                      //new Item { SKU='D'}
+                                      };
+        }
 }
