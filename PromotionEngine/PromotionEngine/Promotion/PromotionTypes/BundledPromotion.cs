b783b37d9d88f5e1d0597ec30c7d53692ce6bc02
akumar65063 <akumar65063@gmail.com>
2021-07-05 10:19:35 +0100

    Modified the method 'GetPromotionDiscount' to claclulate the discount amount for bundled promotion

--- PromotionEngine/PromotionEngine/Promotion/PromotionTypes/BundledPromotion.cs
+++ PromotionEngine/PromotionEngine/Promotion/PromotionTypes/BundledPromotion.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using PromotionEngine.Models;
+
 namespace PromotionEngine.Promotion.PromotionTypes
 {
     public class BundledPromotion:IPromotion
@@ -18,9 +20,28 @@ namespace PromotionEngine.Promotion.PromotionTypes
 
         //Returns the amount to be discounted after applying
         //this promotion
-        public double GetPromotionDiscount()
+        public double GetPromotionDiscount(Order order)
         {
-            return 0;
+            double priceOfItem, amountWithoutDiscount, amountWithDiscount, totalDiscount;
+            int countOfItemInOrder, numberOfBundles;
+
+            priceOfItem = order.Items.Find(x => x.SKU == _itemSKU).Price;
+
+            countOfItemInOrder = order.Items.FindAll(x => x.SKU == _itemSKU).Count;
+
+            numberOfBundles = countOfItemInOrder / _itemCountInOneBundle;
+
+            if (numberOfBundles == 0)
+                return 0;
+
+            amountWithoutDiscount = numberOfBundles * _itemCountInOneBundle * priceOfItem;
+
+            amountWithDiscount = _priceForBundle * numberOfBundles;
+
+            totalDiscount = amountWithoutDiscount - amountWithDiscount;
+
+
+            return totalDiscount;
 
         }
     }
